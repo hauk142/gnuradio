32a61d5a038e8dc78c72f985eda7240e55056b2e
Marc Lichtman <mlichtman@perspectalabs.com>
2020-01-05 21:36:19 -0500

    block id is found when block is dropped into canvas, and cleaned up code

--- grc/guipyqt/window.py
+++ grc/guipyqt/window.py
@@ -1,14 +1,25 @@
 import sys
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QTreeView, QAbstractItemView
-from PyQt5.QtCore import QSize, QDataStream, QVariant, Qt
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QTreeView, QAbstractItemView, QGraphicsView, QGraphicsScene
+from PyQt5.QtCore import QSize, QDataStream, QVariant, Qt, QRectF
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
 import six
 import time
 
-class MyQTreeView(QTreeView):
-    def __init__(self):
-        QTreeView.__init__(self)
+WINDOW_SIZE = 840, 600
+
+# Main Canvas
+class MyQGraphicsScene(QGraphicsScene):
+    def __init__(self, platform):
+        QGraphicsScene.__init__(self)
+        self.platform = platform
+        
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+        else:
+            event.ignore()    
 
     def dragMoveEvent(self, event):
         if event.mimeData().hasUrls:
@@ -16,25 +27,7 @@ class MyQTreeView(QTreeView):
             event.accept()
         else:
             event.ignore()
-
-    def dropEvent(self, event):
-        QTreeView.dropEvent(self, event)
-        if event.mimeData().hasUrls:
-            event.setDropAction(QtCore.Qt.CopyAction)
-            event.accept()
-            # to get a list of files:
-            drop_list = []
-            for url in event.mimeData().urls():
-                drop_list.append(str(url.toLocalFile()))
-            # handle the list here
-        else:
-            event.ignore()
-
-
-class BlockModel(QStandardItemModel):
-    def __init__(self):
-        QStandardItemModel.__init__(self)
-
+            
     def decode_data(self, bytearray):
         data = []
         item = {}
@@ -50,22 +43,30 @@ class BlockModel(QStandardItemModel):
                 item[Qt.ItemDataRole(key)] = value
             data.append(item)
         return data
+                    
+    def dropEvent(self, event):
+        QGraphicsScene.dropEvent(self, event)
+        if event.mimeData().hasUrls:
+            data = event.mimeData()
+            if data.hasFormat('application/x-qabstractitemmodeldatalist'):
+                bytearray = data.data('application/x-qabstractitemmodeldatalist')
+                data_items = self.decode_data(bytearray)
 
-    def itemData(self, x):
-        return {0: x.data()}
-
-    def dropMimeData(self, data, action, row, column, parent):
-        if data.hasFormat('application/x-qabstractitemmodeldatalist'):
-            bytearray = data.data('application/x-qabstractitemmodeldatalist')
-            data_items = self.decode_data(bytearray)
-
-            # Assuming that we get at least one item, and that it defines text that we can display.
-            text = data_items[0][Qt.DisplayRole]
-            print(text.value())
-
-            return True
+                # Pull out label text and use it to find block's key
+                label_text = data_items[0][Qt.DisplayRole].value() # Assuming that we get at least one item, and that it defines text that we can display.
+                # Pretty inefficient way of doing it, but on Marc's computer it takes less than 1ms, and we can always improve it later
+                for block in six.itervalues(self.platform.blocks):
+                    if block.label == label_text:
+                        break
+                print(block.key)
+                
+                
+                event.setDropAction(QtCore.Qt.CopyAction)
+                event.accept()
+            else:
+                return QStandardItemModel.dropMimeData(self, data, action, row, column, parent)
         else:
-            return QStandardItemModel.dropMimeData(self, data, action, row, column, parent)
+            event.ignore()
 
 
 class MyWindow(QMainWindow):
@@ -79,21 +80,39 @@ class MyWindow(QMainWindow):
 
         centralWidget = QWidget()
         self.setCentralWidget(centralWidget)
-
+        
+        # Set up layout
+        gridLayout = QGridLayout(centralWidget)
+        centralWidget.setLayout(gridLayout)        
+        
         # Create QStandardItemModel to represent blocks that will show up in the TreeView
-        block_model = BlockModel() #QStandardItemModel()
+        block_model = QStandardItemModel()
 
         # TreeView
-        self.tree = MyQTreeView() # MIGHT NEED A self IN THE ARG, I SEE IT EVERYWHERE ELSE BUT NOT SURE WHAT IT DOES
+        self.tree = QTreeView(self)
         self.tree.setModel(block_model)
         self.tree.setHeaderHidden(True)
         self.tree.setDragEnabled(True) # needed to be able to click and hold on one of the rows
-        self.tree.setDragDropMode(QAbstractItemView.DragDrop) # definitely needed, options are NoDragDrop, DragOnly, DropOnly, DragDrop, InternalMove
+        self.tree.setDragDropMode(QAbstractItemView.DragOnly)
+        gridLayout.addWidget(self.tree, 0, 1)
+        
+        # Main Canvas
+        self.view = QGraphicsView()
+        self.scene = MyQGraphicsScene(self.platform)
+        self.scene.acceptDrops = True
+        self.scene.setSceneRect(QRectF(0, 0, *WINDOW_SIZE))
+ 
+        #felt = QBrush(QPixmap(os.path.join('images','felt.png')))
+        #self.scene.setBackgroundBrush(felt)
 
-        gridLayout = QGridLayout(centralWidget)
-        centralWidget.setLayout(gridLayout)
-        gridLayout.addWidget(self.tree, 0, 0)
+        #name = QGraphicsPixmapItem()
+        #name.setPixmap(QPixmap(os.path.join('images','ronery.png')))
+        #name.setPos(QPointF(170, 375))
+        #self.scene.addItem(name)
 
+        self.view.setScene(self.scene)
+        gridLayout.addWidget(self.view, 0, 0)        
+        
         # Create tree/dict structure out of list of blocks. Takes ~1 ms on Marc's machine
         block_tree = {}
         for block in six.itervalues(self.platform.blocks):
@@ -134,9 +153,10 @@ class MyWindow(QMainWindow):
             child_item = QStandardItem(child)
             child_item.setEditable(False)
             child_item.setDragEnabled(True)
-            child_item.setDropEnabled(True)
             parent.appendRow(child_item)
             if bool(children): # if dict is not empty it will return True
+                parent.setDragEnabled(False) # categories should not be draggable
                 self._populateTree(children[child], child_item)
+                
 
 
